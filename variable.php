8f2ad0e5348d4303d1345e5e84675f7d66665b66
Fadhila-Zahra <zahrafadhila2907@gmail.com>
2022-03-09 14:58:58 +0700

    membuat file variable.php

---
+++ variable.php
@@ -0,0 +1,17 @@
+<?php
+
+    $x = 4;
+    $y = 2;
+    $hasil = $x + $y ;
+   // echo $hasil;
+    echo $x. " + " .$y. " = " .$hasil;
+
+    echo "<br>";
+    $hasil = $x - $y ;
+    echo $x. " - " .$y. " = " .$hasil;
+
+    echo "<br>";
+    $hasil = $x * $y ;
+    echo $x. " * " .$y. " = " .$hasil;
+
+?>
