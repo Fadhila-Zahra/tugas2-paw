31ad0f7bc2dce526ac467d14a0df4b402ea51f42
Fadhila-Zahra <zahrafadhila2907@gmail.com>
2022-03-09 15:30:18 +0700

    menambah contoh dengan variabel baru

--- variable.php
+++ variable.php
@@ -1,5 +1,5 @@
 <?php
-
+    echo "contoh 2 variabel <br>";
     $x = 4;
     $y = 2;
     $hasil = $x + $y ;
@@ -14,4 +14,29 @@
     $hasil = $x * $y ;
     echo $x. " * " .$y. " = " .$hasil;
 
+    echo "<br> <br> contoh 3 variabel";
+    // contoh 2
+    echo "<br>";
+    $a = 4;
+    $b = 2;
+    $c = 1;
+    $hasil = $a + $b + $c;
+    //echo $hasil;
+    echo $a. " + " .$b. " + ". $c." = " .$hasil;
+
+    echo "<br>";
+    $a = 4;
+    $b = 2;
+    $c = 1;
+    $hasil = $a - $b - $c;
+    //echo $hasil;
+    echo $a. " - " .$b. " - ". $c." = " .$hasil;
+
+    echo "<br>";
+    $a = 4;
+    $b = 2;
+    $c = 1;
+    $hasil = $a * $b * $c;
+    //echo $hasil;
+    echo $a. " * " .$b. " * ". $c." = " .$hasil;
 ?>
